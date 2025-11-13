bdea61e9125c0d37b5756ed35416099a93e93019
Gatik Rajput <grajput2@asu.edu>
2025-11-13 20:21:24 +0530

    Update package.json to fix start:dev command spacing, import TodoModule in app.module.ts, and modify greeting in AppService

---
+++ src/todo/todo.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
+
+@Injectable()
+export class TodoService {
+  private todos: Todo[] = [];
+  private nextId = 1;
+
+  create(createTodoDto: CreateTodoDto): Todo {
+    const todo: Todo = {
+      id: this.nextId++,
+      title: createTodoDto.title,
+      description: createTodoDto.description,
+      completed: createTodoDto.completed ?? false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    this.todos.push(todo);
+    return todo;
+  }
+
+  findAll(): Todo[] {
+    return this.todos;
+  }
+
+  findOne(id: number): Todo {
+    const todo = this.todos.find((t) => t.id === id);
+    if (!todo) {
+      throw new NotFoundException(`Todo with ID ${id} not found`);
+    }
+    return todo;
+  }
+
+  update(id: number, updateTodoDto: UpdateTodoDto): Todo {
+    const todo = this.findOne(id);
+    Object.assign(todo, updateTodoDto, { updatedAt: new Date() });
+    return todo;
+  }
+
+  remove(id: number): void {
+    const index = this.todos.findIndex((t) => t.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Todo with ID ${id} not found`);
+    }
+    this.todos.splice(index, 1);
+  }
+}
