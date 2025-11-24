a623e9c316eaf6b6510cd16fcade8db82cd8da76
Gatik Rajput <grajput2@asu.edu>
2025-11-24 20:37:31 +0530

    Add @nestjs/throttler for rate limiting in app.module.ts and update package.json and pnpm-lock.yaml to include the new dependency.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
@@ -7,8 +9,25 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [PrismaModule, TodoModule, AuthModule, ReportsModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60_000,
+        limit: 100,
+      },
+    ]),
+    PrismaModule,
+    TodoModule,
+    AuthModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
